a5a1280bf947d173eafa26fdad9feb105ed68c0a
Boss921832137 <89962703+Boss921832137@users.noreply.github.com>
2021-09-02 17:29:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,29 @@
-function now(){
-    for (let i = 1; i < 7; i++) {
-      let div = document.getElementById("stage"+i)
-      let time = new Date()
-      switch(i){
-        case 1:
-          time.setMinutes(time.getMinutes()+105+14)
-          break
-        case 2:
-          time.setMinutes(time.getMinutes()+195+14)
-          break
-        case 3:
-          time.setMinutes(time.getMinutes()+285+14)
-          break
-        case 4:
-          time.setMinutes(time.getMinutes()+375+14)
-          break
-        case 5:
-          time.setMinutes(time.getMinutes()+465+14)
-          break
-        case 6:
-          time.setMinutes(time.getMinutes()+555)
-          break
-        
-     }
-      div.innerHTML = time.getHours() + ':' + time.getMinutes()
-    }
-    console.log(noww)
-}
-
-function on(date){  
-  for (let i = 1; i < 7; i++) {
-    let time = new Date()
-    let [hours, minutes] = date.split(':');
-
-    time.setHours(+hours); // Set the hours, using implicit type coercion
-    time.setMinutes(minutes); // You can pass Number or String. It doesn't really matter
-
-    let div = document.getElementById("stage"+i)
-      switch(i){
-        case 1:
-          time.setMinutes(time.getMinutes()-105-14)
-          break
-        case 2:
-          time.setMinutes(time.getMinutes()-195-14)
-          break
-        case 3:
-          time.setMinutes(time.getMinutes()-285-14)
-          break
-        case 4:
-          time.setMinutes(time.getMinutes()-375-14)
-          break
-        case 5:
-          time.setMinutes(time.getMinutes()-465-14)
-          break
-        case 6:
-          time.setMinutes(time.getMinutes()-555-14)
-          break  
-     }
-     div.innerHTML = time.getHours() + ':' + time.getMinutes()
-    }
-}
-
-window.onload = function() {
-
-  new Rolldate({
-
-    el:'#times',
-    format: 'hh:mm',
-    confirm: function(date){
-      on(date)
-    },
-    lang: { 
-      title: 'Выберите время', 
-      cancel: 'Отмена', 
-      confirm: 'Применить', 
-      hour: '', 
-      min: ''
-    }
-
+$(function(){
+  $('#sleepcalc').combodate({
+    customClass: 'form-control'
+  });  
+  
+  $('#sleepcalc, .hour, .minute').change( function() {
+    updatetime()
   })
-}
+  
+  
+  function updatetime(){
+    var interval = moment.duration("01:30:00")
+    var secondinterval = moment.duration("03:00:00")
+    var selectedTime = $('#sleepcalc').combodate('getValue', null)
+    var time = moment.duration("09:15:00")
+    var date = moment(selectedTime)
+    date.subtract(time)
+    result1 = moment(date).format("h:mm a")
+    result2 = moment(date).add(interval).format("h:mm a")
+    result3 = moment(date).add(secondinterval).format("h:mm a")
+    $('#result1').text(result1).addClass('loaded')
+    $('#result2').text(result2).addClass('loaded')
+    $('#result3').text(result3).addClass('loaded')
+    setTimeout(function() {
+        $('#result1, #result2, #result3').removeClass('loaded');
+    }, 1500)
+  }              
+  updatetime()
+});
