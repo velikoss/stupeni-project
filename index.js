d54013f650447fb34d1d268e74a5c15459c2d7a6
Velikiy Kirill <50528703+velikoss@users.noreply.github.com>
2021-09-02 17:40:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(function(){
+   
   $('#sleepcalc').combodate({
     customClass: 'form-control'
   });  
@@ -11,13 +12,17 @@ $(function(){
   function updatetime(){
     var interval = moment.duration("01:30:00")
     var secondinterval = moment.duration("03:00:00")
+    
     var selectedTime = $('#sleepcalc').combodate('getValue', null)
+    
     var time = moment.duration("09:15:00")
     var date = moment(selectedTime)
     date.subtract(time)
+
     result1 = moment(date).format("h:mm a")
     result2 = moment(date).add(interval).format("h:mm a")
     result3 = moment(date).add(secondinterval).format("h:mm a")
+
     $('#result1').text(result1).addClass('loaded')
     $('#result2').text(result2).addClass('loaded')
     $('#result3').text(result3).addClass('loaded')
